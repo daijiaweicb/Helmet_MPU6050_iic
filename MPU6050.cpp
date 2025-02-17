9b8cc32d5b9a4b1c55e611ba4e603fb30df2d169
zyx <2511305125@qq.com>
2025-02-17 15:13:07 +0000

    Test

--- MPU6050.cpp
+++ MPU6050.cpp
@@ -36,7 +36,7 @@ SensorData readMPU6050(IIC &iic) {
     SensorData data;
 
     // 读取14字节数据（从0x3B开始）
-    // iic.iic_writeRegister(0x43, 0x00); // 设置起始寄存器地址
+    iic.iic_writeRegister(0x43, 0x00); // 设置起始寄存器地址
     ssize_t bytesRead = read(0x43, buffer, 6);
     if (bytesRead != 6) {
         throw std::runtime_error("读取传感器数据失败");
