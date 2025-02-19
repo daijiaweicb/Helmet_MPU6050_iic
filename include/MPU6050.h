a2bb2bc23a9d6b940a8613b7be9072ffb5cd9465
zyx <2511305125@qq.com>
2025-02-19 10:51:06 +0000

    NewClass

--- include/MPU6050.h
+++ include/MPU6050.h
@@ -1,5 +1,5 @@
-#ifndef _MPU6050_H
-#define _MPU6050_H
+#ifndef MPU6050_H
+#define MPU6050_H
 
 #include "iic.h"
 #include <iostream>
@@ -10,35 +10,14 @@
 #include <cstring>
 #include <cstdint>
 
-class Kalman : public MPU
-{
-    public:
-    struct KalmanFilter
-    {
-        public:
-        float angle;      // 估计的角度
-        float bias;       // 估计的陀螺仪零偏
-        float P[2][2];    // 误差协方差矩阵
-        float Q_angle;    // 过程噪声方差（角度）
-        float Q_bias;     // 过程噪声方差（零偏）
-        float R_measure;  // 观测噪声方差
-    };  
-
-    void initKalmanFilter(KalmanFilter &kf);
-    float kalmanUpdate(KalmanFilter &kf, float newRate, float dt, float measuredAngle);
-    AngleData calculateAngle(const SensorData &data, float dt, const AngleData &prev, 
-                           const AngleData &calib, KalmanFilter &kfRoll, KalmanFilter &kfPitch);
-};
-
 class MPU
 {
-    public:
+public:
     struct SensorData
     {
         float gyroX, gyroY, gyroZ;
         float accelX, accelY, accelZ;
     };
-
 
     struct AngleData 
     {
@@ -46,13 +25,32 @@ class MPU
         float gyroBiasX, gyroBiasY, gyroBiasZ;
     };
 
-    void calibrateSensors(IIC &iic, AngleData &calib, int samples) ;
+    virtual ~MPU() = default; // 让基类有一个虚析构函数
+
+    void calibrateSensors(IIC &iic, AngleData &calib, int samples);
     void initMPU6050(IIC &iic);
     SensorData readMPU6050(IIC &iic);
-    float getAccRoll(float accelY, float accelZ) ;
+    float getAccRoll(float accelY, float accelZ);
     float getAccPitch(float accelX, float accelY, float accelZ);
 };
 
+class Kalman : public MPU
+{
+public:
+    struct KalmanFilter
+    {
+        float angle;      // 估计的角度
+        float bias;       // 估计的陀螺仪零偏
+        float P[2][2];    // 误差协方差矩阵
+        float Q_angle;    // 过程噪声方差（角度）
+        float Q_bias;     // 过程噪声方差（零偏）
+        float R_measure;  // 观测噪声方差
+    };
 
-#endif
+    void initKalmanFilter(KalmanFilter &kf);
+    float kalmanUpdate(KalmanFilter &kf, float newRate, float dt, float measuredAngle);
+    AngleData calculateAngle(const SensorData &data, float dt, const AngleData &prev,
+                             const AngleData &calib, KalmanFilter &kfRoll, KalmanFilter &kfPitch);
+};
 
+#endif // MPU6050_H
