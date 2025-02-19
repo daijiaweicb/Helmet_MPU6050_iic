5097b0017a50e380478ae17ef4754da490fc419e
zyx <2511305125@qq.com>
2025-02-19 12:17:15 +0000

    ChangeI2C

--- include/iic.h
+++ include/iic.h
@@ -1,6 +1,11 @@
 #ifndef IIC_H
 #define IIC_H
 
+/**
+ * The I2C is controlled by a kernel driver
+ * Each I2C adapter gets a number, for Raspberry pi, the number of the adapter is 1
+ */
+
 #include <iostream>
 #include <fcntl.h>
 #include <linux/i2c-dev.h>
@@ -12,10 +17,38 @@
 
 class IIC {
 public:
+    /**
+     * construct of the class
+     */
     IIC(int adapter) : adapter_nr(adapter), file(-1) {}
+
+    
+    /**
+     * Open the I2C device
+     * In this case, open the MPU 6050
+     */
     void iic_open();
+
+    
+    /**
+     * Close the I2C device
+     */
     void iic_close();
+
+    
+    /**
+     * 
+     * @param  {uint8_t} reg     : 
+     * @param  {uint8_t*} buffer : 
+     * @param  {size_t} length   : 
+     * @return {bool}            : 
+     */
     bool readRegisters(uint8_t reg, uint8_t* buffer, size_t length);
+    /**
+     * 
+     * @param  {uint8_t} reg   : 
+     * @param  {uint8_t} value : 
+     */
     void iic_writeRegister(uint8_t reg, uint8_t value);
     int file;
 
