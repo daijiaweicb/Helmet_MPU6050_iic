a7b7b415d357ef5f7e784f370f3d6e9c682b0a78
zyx <2511305125@qq.com>
2025-03-05 10:00:20 +0000

    DEBUG

--- src/Test.cpp
+++ src/Test.cpp
@@ -2,15 +2,15 @@
 
 void test()
 {
-    // IIC iic(1);
+    IIC iic(1);
     MPU mpu;
     MyMPU mypu;
     GetMPU gmu;
 
-    // iic.iic_open();
-    mpu.beginMPU6050();
+    iic.iic_open();
+    // mpu.beginMPU6050();
     gmu.RegisterSetting(&mypu);
 
     
-    // iic.iic_close();
+    iic.iic_close();
 }
